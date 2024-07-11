26e11cf45c9182c35abf5a67f84c129d50ecbe81
Eccentric-art <pmb1986@yahoo.com>
2024-07-11 13:04:54 +0100

    first commit

---
+++ eccentric.js
@@ -0,0 +1,51 @@
+// console.log("brown eccentric");
+
+// let gender;
+// gender = "male";
+// console.log(gender);
+
+// let firstname = "brown";
+// const lastname = "eccentric";
+// const age = 30;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log("my name is" + firstname + lastname + "i am " + age + "years old");
+// console.log(`my name is ${firstname} ${lastname} i am ${age}years old, i am the ${isAdmin}`);
+// console.log(firstname.toLocaleLowerCase());
+
+// const title = "welcome to my home";
+// console.log(title .length);
+// console.log(title.toLocaleUpperCase()); to make it a capital letter
+// console.log(title.toLocaleLowerCase());
+// console.log(title.split());
+// console.log(title.split(""));
+
+// const title = "welcome, laptop, ict, router";
+// console.log(title.split(","));
+
+// const students = ["web", "home", "kitchen", 6, true];
+// console.log(students);
+// const students = ["web", "home", "kitchen", 6, true];
+// students.push("table","chair")to add a value
+// students[5]="table" another way of adding a value
+// students.unshift("computer")to add value to start from the start
+// students.pop() to remove a value
+// console.log(students);
+
+const person = {
+  firstname: "brown",
+  lastname: "eccentric",
+  score: 40.5,
+  isAdmin: false,
+  hobbies: ["cooking", "betting", "boxing"],
+  address: {
+    city: "owerri",
+    lga: "owerri-west",
+    postalcode: 46013,
+  },
+};
+console.log(person);
+// const { firstname, lastname } = person;
+// console.log(firstname, lastname);
+// console.log(JSON.stringify(person));
